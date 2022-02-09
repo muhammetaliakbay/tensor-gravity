80c390fae74a7532b8886d904bbf35144b47daa1
Muhammet Ali AKBAY <me@muhammetaliakbay.com>
2022-02-09 17:45:05 +0100

    Combined XYZ into single tensor

--- gravity.py
+++ gravity.py
@@ -1,83 +1,67 @@
+from numpy import Infinity
 import tensorflow as tf
 from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
+import matplotlib.cm as cm
+from tqdm import tqdm
 
 @tf.function
-def vector_delta_combination(a):
+def delta_combination(a):
     expand = tf.expand_dims(a, 0)
-    tile = tf.tile(expand, (a.shape[0], 1))
-    transpose = tf.transpose(tile)
+    tile = tf.tile(expand, (a.shape[0], 1, 1))
+    transpose = tf.transpose(tile, perm=[1,0,2])
     return transpose - tile
 
 @tf.function
-def zero_diagonal(a):
-    return tf.linalg.set_diag(
-        a,
-        tf.zeros(a.shape[:-1]),
-    )
+def nan_to_zero(a):
+    return tf.where(tf.math.is_nan(a), tf.zeros_like(a), a)
 
-particles = 1000
+particles = 10000
 G = 10
 mass = 0.0001
 
 time_delta = 0.05
-time_factor = 0.25
+time_factor = 0.5
 
-locations_x = tf.Variable(tf.random.uniform((particles,), -1, 1, seed = 1))
-locations_y = tf.Variable(tf.random.uniform((particles,), -1, 1, seed = 2))
-locations_z = tf.Variable(tf.random.uniform((particles,), -1, 1, seed = 3))
-
-velocities_x = tf.Variable(tf.random.uniform((particles,), -1, 1, seed = 1))
-velocities_y = tf.Variable(tf.random.uniform((particles,), -1, 1, seed = 2))
-velocities_z = tf.Variable(tf.random.uniform((particles,), -1, 1, seed = 3))
+locations = tf.Variable(tf.random.uniform((particles, 3), -1, 1, seed = 1))
+velocities = tf.Variable(tf.random.uniform((particles, 3), -1, 1, seed = 1))
+colors = tf.Variable(tf.zeros((particles,)))
 
 @tf.function
-def calculate(locations_x, locations_y, locations_z, velocities_x, velocities_y, velocities_z):
-    distances_x = vector_delta_combination(locations_x)
-    distances_y = vector_delta_combination(locations_y)
-    distances_z = vector_delta_combination(locations_z)
+def calculate():
+    distances = delta_combination(locations)
 
-    distances = tf.sqrt(tf.square(distances_x) + tf.square(distances_y) + tf.square(distances_z))
+    abs_distances = tf.sqrt(tf.reduce_sum(tf.square(distances), axis=-1))
 
-    forces = ( G * ( (mass * mass) / distances ) )
+    forces = ( G * ( (mass * mass) / abs_distances ) )
 
     accelerations = forces / mass
 
-    accelerations_x = -tf.reduce_sum(zero_diagonal( (accelerations * distances_x) / distances ), -1)
-    accelerations_y = -tf.reduce_sum(zero_diagonal( (accelerations * distances_y) / distances ), -1)
-    accelerations_z = -tf.reduce_sum(zero_diagonal( (accelerations * distances_z) / distances ), -1)
+    peer_accelerations = (tf.expand_dims(accelerations, -1) * distances) / tf.expand_dims(abs_distances, -1)
+    peer_accelerations = nan_to_zero(peer_accelerations)
 
-    new_velocities_x = velocities_x + accelerations_x*time_delta*time_factor
-    new_velocities_y = velocities_y + accelerations_y*time_delta*time_factor
-    new_velocities_z = velocities_z + accelerations_z*time_delta*time_factor
+    accelerations = -tf.reduce_sum(peer_accelerations, -2)
 
-    new_locations_x = locations_x + new_velocities_x*time_delta*time_factor
-    new_locations_y = locations_y + new_velocities_y*time_delta*time_factor
-    new_locations_z = locations_z + new_velocities_z*time_delta*time_factor
+    new_velocities = velocities + accelerations*time_delta*time_factor
+
+    new_locations = locations + new_velocities*time_delta*time_factor
     
-    return new_locations_x, new_locations_y, new_locations_z, new_velocities_x, new_velocities_y, new_velocities_z
+    locations.assign(new_locations)
+    velocities.assign(new_velocities)
+
+    colors.assign(tf.sqrt(tf.reduce_sum(tf.square(new_velocities), axis=-1)))
 
 def animate(frame):
-    (
-        new_locations_x, new_locations_y, new_locations_z,
-        new_velocities_x, new_velocities_y, new_velocities_z,
-    ) = calculate(
-        locations_x, locations_y, locations_z,
-        velocities_x, velocities_y, velocities_z,
-    )
-
-    locations_x.assign(new_locations_x)
-    locations_y.assign(new_locations_y)
-    locations_z.assign(new_locations_z)
-
-    velocities_x.assign(new_velocities_x)
-    velocities_y.assign(new_velocities_y)
-    velocities_z.assign(new_velocities_z)
+    calculate()
 
     ax.view_init(30, frame * 0.25)
 
-    graph.set_data(locations_x.numpy(), locations_y.numpy())
-    graph.set_3d_properties(locations_z.numpy())
+    graph._offsets3d=(
+        locations[:,0].numpy(),
+        locations[:,1].numpy(),
+        locations[:,2].numpy(),
+    )
+    graph.set_color(cm.hot(colors.numpy()))
     title.set_text('3D Test, time={}'.format(frame))
     return title, graph, 
 
@@ -85,9 +69,37 @@ fig = plt.figure(figsize=(5, 5))
 ax = fig.add_subplot(projection='3d')
 title = ax.set_title('Gravity')
 
-graph, = ax.plot(locations_x.numpy(), locations_y.numpy(), locations_z.numpy(), linestyle="", marker="o", ms=0.75)
+ax.set_facecolor('black')
+ax.grid(False)
+ax.set_xticks([])
+ax.set_yticks([])
+ax.set_zticks([])
+ax.xaxis.set_pane_color((1.0, 1.0, 1.0, 0))
+ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0))
+ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0))
 
-ani = FuncAnimation(fig, animate, frames=int(30/time_delta), interval=1.0/time_delta, blit=False)
-# ani.save("gravity.mp4")
+graph = ax.scatter(
+    locations[:,0].numpy(),
+    locations[:,1].numpy(),
+    locations[:,2].numpy(),
+    s=0.5
+)
 
-plt.show()
+frames = int(60/time_delta)
+
+ani = FuncAnimation(
+    fig,
+    animate,
+    frames=frames,
+    interval=1.0/time_delta,
+    blit=False,
+)
+
+with tqdm(total=frames) as pbar:
+    ani.save(
+        "gravity.mp4",
+        fps=1.0/time_delta,
+        progress_callback=lambda i, n: pbar.update(i)
+    )
+
+# plt.show()
