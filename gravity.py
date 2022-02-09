db10b7cb613c61ba1e7416235745a73964c23f0e
Muhammet Ali AKBAY <me@muhammetaliakbay.com>
2022-02-09 19:34:15 +0100

    Multi variable batches

--- gravity.py
+++ gravity.py
@@ -8,10 +8,10 @@ import matplotlib.cm as cm
 from tqdm import tqdm
 
 @tf.function
-def delta_combination(a, start, end):
+def delta_combination(a, slice):
     expand_v = tf.expand_dims(a, 0)
-    expand_h = tf.expand_dims(a[start:end], 1)
-    tile_v = tf.tile(expand_v, (end-start, 1, 1))
+    expand_h = tf.expand_dims(slice, 1)
+    tile_v = tf.tile(expand_v, (slice.shape[0], 1, 1))
     tile_h = tf.tile(expand_h, (1, a.shape[0], 1))
     return tile_h - tile_v
 
@@ -19,57 +19,75 @@ def delta_combination(a, start, end):
 def nan_to_zero(a):
     return tf.where(tf.math.is_nan(a), tf.zeros_like(a), a)
 
-particles = 10000
+batches = 10
+particles_per_batch = 250
+
 G = 10
-mass = 0.0001
+mass = 0.00001
 
 time_delta = 0.05
 time_factor = 0.5
 
-locations = tf.Variable(tf.random.uniform((particles, 3), -1, 1, seed = 1))
-velocities = tf.Variable(tf.random.uniform((particles, 3), -0.1, 0.1, seed = 2))
-colors = tf.Variable(tf.zeros((particles,)))
+location_variables = [
+    tf.Variable(tf.random.uniform((particles_per_batch, 3), -1, 1, seed = i))
+    for i in range(batches)
+]
+velocity_variables = [
+    tf.Variable(tf.random.uniform((particles_per_batch, 3), -0.1, 0.1, seed = i))
+    for i in range(batches)
+]
+acceleration_variables = [
+    tf.Variable(tf.zeros((particles_per_batch, 3)))
+    for i in range(batches)
+]
+color_variables = [
+    tf.Variable(tf.zeros((particles_per_batch,)))
+    for i in range(batches)
+]
 
 @tf.function
-def calculate(start, end):
-    distances = delta_combination(locations, start, end)
+def update():
+    for i in range(batches):
+        location_variables[i].assign_add(velocity_variables[i]*time_delta*time_factor)
+        velocity_variables[i].assign_add(acceleration_variables[i]*time_delta*time_factor)
+
+@tf.function
+def calculate(location_variable, velocity_variable, acceleration_variable, color_variable):
+    locations = tf.concat(location_variables, 0)
+
+    distances = delta_combination(locations, location_variable)
 
     abs_distances = tf.sqrt(tf.reduce_sum(tf.square(distances), axis=-1))
 
     forces = ( G * ( (mass * mass) / abs_distances ) )
 
-    accelerations = forces / mass
-
-    peer_accelerations = (tf.expand_dims(accelerations, -1) * distances) / tf.expand_dims(abs_distances, -1)
+    peer_accelerations = (tf.expand_dims(forces / mass, -1) * distances) / tf.expand_dims(abs_distances, -1)
     peer_accelerations = nan_to_zero(peer_accelerations)
 
-    accelerations = -tf.reduce_sum(peer_accelerations, -2)
+    acceleration_variable.assign(-tf.reduce_sum(peer_accelerations, -2))
 
-    new_velocities = velocities[start:end] + accelerations*time_delta*time_factor
-    
-    velocities.assign(tf.concat([velocities[:start], new_velocities, velocities[end:]], 0))
-
-    new_locations = locations + velocities*time_delta*time_factor
-    locations.assign(new_locations)
-
-    new_colors = tf.sqrt(tf.reduce_sum(tf.square(new_velocities), axis=-1))
-    colors.assign(tf.concat([colors[:start], new_colors, colors[end:]], 0))
-
+    new_colors = tf.sqrt(tf.reduce_sum(tf.square(velocity_variable), axis=-1))
+    color_variable.assign(new_colors)
 
 def animate(frame):
-    length = 100
-    start = (frame * length) % particles
-    end = start + length
-    calculate(start, end)
+    update()
+    calculate(
+        location_variables[frame % batches],
+        velocity_variables[frame % batches],
+        acceleration_variables[frame % batches],
+        color_variables[frame % batches],
+    )
 
     ax.view_init(30, frame * 0.25)
+
+    locations = tf.concat(location_variables, 0)
 
     graph._offsets3d=(
         locations[:,0].numpy(),
         locations[:,1].numpy(),
         locations[:,2].numpy(),
     )
-    graph.set_color(cm.hot(colors.numpy()))
+    graph.set_color(cm.hot(tf.concat(color_variables, 0).numpy()))
     title.set_text('3D Test, time={}'.format(frame))
     return title, graph, 
 
@@ -87,9 +105,9 @@ ax.yaxis.set_pane_color((1.0, 1.0, 1.0, 0))
 ax.zaxis.set_pane_color((1.0, 1.0, 1.0, 0))
 
 graph = ax.scatter(
-    locations[:,0].numpy(),
-    locations[:,1].numpy(),
-    locations[:,2].numpy(),
+    [],
+    [],
+    [],
     s=0.5
 )
 
