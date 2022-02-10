d6b3460a99efc2a86ff0697f40ab069d5e2e6f0d
Muhammet Ali AKBAY <me@muhammetaliakbay.com>
2022-02-10 11:54:08 +0100

    Remove inner batching, and use threading with queue

--- gravity.py
+++ gravity.py
@@ -1,4 +1,6 @@
 import os
+from queue import Queue
+import threading
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
 
 import tensorflow as tf
@@ -7,7 +9,7 @@ from matplotlib.animation import FuncAnimation
 import matplotlib.cm as cm
 from tqdm import tqdm
 
-@tf.function
+# @tf.function
 def delta_combination(a, peer):
     expand_v = tf.expand_dims(a, 0)
     expand_h = tf.expand_dims(peer, 1)
@@ -29,17 +31,14 @@ location_variables = [
     for i in range(batches)
 ]
 velocity_variables = [
-    tf.Variable(location_variables[i].numpy())
-    # tf.Variable(tf.random.uniform((particles_per_batch, 3), -0.1, 0.1, seed = i + 1_000_000))
+    # tf.Variable(location_variables[i].numpy())
+    tf.Variable(tf.random.uniform((particles_per_batch, 3), -1, 1, seed = i + 1_000_000))
     for i in range(batches)
 ]
 acceleration_variables = [
-    [
         tf.Variable(tf.zeros((particles_per_batch, 3)))
         for _ in range(batches)
     ]
-    for _ in range(batches)
-]
 color_variables = [
     tf.Variable(tf.zeros((particles_per_batch,)))
     for i in range(batches)
@@ -50,42 +49,55 @@ def update():
     for i in range(batches):
         color_variables[i].assign(tf.sqrt(tf.reduce_sum(tf.square(velocity_variables[i]), axis=-1)))
         location_variables[i].assign_add(velocity_variables[i]*time_delta*time_factor)
-        for j in range(batches):
-            velocity_variables[i].assign_add(acceleration_variables[i][j]*time_delta*time_factor)
+        velocity_variables[i].assign_add(acceleration_variables[i]*time_delta*time_factor)
 
 @tf.function
-def calculate(peer_location_variable, location_variable, acceleration_variable):
-    distances = delta_combination(peer_location_variable, location_variable)
+def calculate(location_variable, acceleration_variable):
+    distances = delta_combination(tf.concat(location_variables, axis=0), location_variable)
 
-    abs_distances = tf.sqrt(tf.reduce_sum(tf.square(distances), axis=-1))
+    squared_distances = tf.reduce_sum(tf.square(distances), axis=-1)
 
-    forces = tf.math.divide_no_nan(G * mass * mass, abs_distances)
+    forces_div_mass_div_squared_distances = tf.math.divide_no_nan(G * mass, squared_distances)
 
-    peer_accelerations = tf.math.divide_no_nan(tf.expand_dims(forces / mass, -1) * distances, tf.expand_dims(abs_distances, -1))
+    peer_accelerations = tf.expand_dims(forces_div_mass_div_squared_distances, -1) * distances
 
     acceleration_variable.assign(-tf.reduce_sum(peer_accelerations, -2))
 
-def animate(real_frame):
-    update()
+queue = Queue(10)
 
-    frame = int(real_frame / batches)
+def producer():
+    with tf.device('/cpu:0'):
+        while True:
+            for index in range(batches):
+    update()
     calculate(
-        location_variables[frame % batches],
-        location_variables[real_frame % batches],
-        acceleration_variables[real_frame % batches][frame % batches],
+                    location_variables[index],
+                    acceleration_variables[index],
+                )
+                locations = tf.concat(location_variables, 0)
+                queue.put(
+                    (
+                        locations[:,0].numpy(),
+                        locations[:,1].numpy(),
+                        locations[:,2].numpy(),
+                        tf.concat(color_variables, 0).numpy(),
+                    )
     )
 
-    ax.view_init(30, real_frame * 0.25)
+threading.Thread(target=producer, daemon=True).start()
 
-    locations = tf.concat(location_variables, 0)
+def animate(frame):
+    # ax.view_init(30, frame * 0.25)
+
+    locations_x, locations_y, locations_z, colors = queue.get()
 
     graph._offsets3d=(
-        locations[:,0].numpy(),
-        locations[:,1].numpy(),
-        locations[:,2].numpy(),
+        locations_x,
+        locations_y,
+        locations_z,
     )
-    graph.set_color(cm.hot(tf.concat(color_variables, 0).numpy()))
-    title.set_text('Gravity, Frame={}'.format(real_frame))
+    graph.set_color(cm.hot(colors))
+    title.set_text('Gravity, Frame={}'.format(frame))
     return title, graph, 
 
 fig = plt.figure(figsize=(5, 5))
